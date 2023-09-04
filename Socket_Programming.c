d49455b1780a9c6338b7461b47a7dd02989a1146
Manav Takoliya <takoliya.manav777@gmail.com>
2023-09-04 20:16:49 +0530

    1

---
+++ Socket_Programming.c
@@ -0,0 +1,55 @@
+//SERVER
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<Winsock2.h>
+
+int main(){
+
+
+    char *ip = "127.0.0.1";
+    int port  = 5566;
+    int server_sock,client_sock;
+    struct sockaddr_in server_addr,client_addr;
+    socklen_t addr_size;
+
+    char buffer[1024];
+
+    int n;
+
+    //Craete TCp socket
+
+    server_sock = socket(AF_INET,SOCK_STREAM,0);
+    if(server_sock<0){
+        perror("[-] Socket Error");
+        exit(1);
+    }
+    printf("[+]TCP SOCKET CREATED.\n");
+
+    memset(&server_addr,"\0",sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = port;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+
+
+    n = bind(server_sock,(struct sockaddr*)&server_addr,sizeof(server_addr));
+
+    if(n<0){
+        perror("[-]Bind Error");
+        exit(1);
+    }
+    printf("[+]Bind to the port number %d\n",port);
+
+    listen(server_sock,5);
+
+    printf("Listening....\n");
+
+    while(1){
+        client_sock = accept(server_sock,(struct sockaddr*)&client_addr,&addr_size);
+        printf("[+]Connected.\n");
+    }
+
+    return 0;
+}
